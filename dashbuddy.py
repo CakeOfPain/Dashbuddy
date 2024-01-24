92d3315c3334e4813146ea3be09e5bc16bf4cf74
Pascal <pascal.jedicke@icloud.com>
2024-01-24 11:59:03 +0100

    Added Widget Template

--- dashbuddy.py
+++ dashbuddy.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
 import plugins.framework as Widget
+import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     Widget.load()
-    return render_template('index.html')
+    widgetView = '<div><h1>widgetView</h1></div>'
+    return render_template('widget.html', title="Testwidget", desc="desc", view=widgetView, utc_dt=datetime.datetime.utcnow())

7df47ea92eee09223438f70e2af4547226b60fa7
Leo Englert <Leo.Englert@outlook.de>
2024-01-24 13:32:21 +0100

    Added Widget Endpoints

--- dashbuddy.py
+++ dashbuddy.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route("/api/widgets", methods=["GET"])
+def getWidgets():
+    return {}, 200
+
+@app.route("/api/widgets", methods=["POST"])
+def postWidgets():
+    return {}, 200

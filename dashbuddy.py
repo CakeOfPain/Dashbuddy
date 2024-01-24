6ab3af89598f6095a3fb158fb6f03d9bfc8cb059
Pascal <pascal.jedicke@icloud.com>
2024-01-24 09:09:50 +0100

    Moved Framework to Rootdirectory

--- dashbuddy.py
+++ dashbuddy.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
+import plugins.framework as Widget
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+    Widget.load()
     return render_template('index.html')

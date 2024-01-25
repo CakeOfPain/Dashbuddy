7f570a6f430791dc841fd2670ebfc17267b43596
Leo Englert <Leo.Englert@outlook.de>
2024-01-25 10:43:13 +0100

    Added list plugins api

--- dashbuddy.py
+++ dashbuddy.py
@@ -9,3 +9,7 @@ pluginManager.manage()
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.get("/api/plugins")
+def listPlugins():
+    return pluginManager.listPluginsJson(), 200

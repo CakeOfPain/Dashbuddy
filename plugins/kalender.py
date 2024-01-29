1f6cc11e92607b4734cdf3ec0e3e5c6bc4632ed8
riedl-sebastian <sebastian.riedl0405@gmail.com>
2024-01-29 13:59:19 +0100

    Fehlerbehebung

---
+++ plugins/kalender.py
@@ -0,0 +1,30 @@
+from datetime import date
+from datetime import datetime
+import calendar
+from flask import render_template
+
+def getDate():
+    aktuellesD=date.today()
+    furweekday=str(aktuellesD)
+    return furweekday, 200
+    
+def getWeekDay():
+    wochentage=["Montag","Dienstag","Mittwoch","Donnerstag","Freitag","Samstag","Sonnatg"]
+    tag=date.weekday()
+    return wochentage[tag], 200
+
+def getCalender():
+    kalenderseite=calendar.HTMLCalendar(calendar.MONDAY)
+    aus=kalenderseite.formatmonth(2024,1)
+    return(aus), 200
+
+def getContent():
+    return render_template("calendar/index.html"), 200
+
+def createPlugin(plugin):
+    plugin.name('calendar')
+    plugin.describe('Zeigt den Kalender')
+    plugin.api('get-Date',getDate)
+    plugin.api('get-Weekday',getWeekDay)
+    plugin.api('get-Calender',getCalender)
+    plugin.content(getContent)

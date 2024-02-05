6f94bf07f025636d2f1051f7f7701cbaf8f50a52
Pascal Jedicke <pascal.jedicke@icloud.com>
2024-02-05 16:06:56 +0100

    Working on DB API

--- README.md
+++ README.md
@@ -4,6 +4,11 @@ Extendable Dashboard written in Python
 ![Lisp](https://www.lisperati.com/lisplogo_warning_256.png)
 
 ## Run the application
+First this
+```bash
+. .venv/bin/activate
+```
+then this
 ```bash
 flask --app dashbuddy run
 ```
--- plugins/timetable.py
+++ plugins/timetable.py
@@ -1,14 +1,24 @@
 from flask import request, render_template
+import logging
 import requests
 import xml.etree.ElementTree as ET
 
-def date2Friendly(timecode):
+dbApiId = "11ee6296e52f2c41be3956420627b6a3"
+dbApiSecret = "5ec59e4b2fdfb645be715940e47c54e0"
+
+logging.basicConfig(format='%(levelname)s - %(asctime)s - %(message)s', level=logging.INFO)
+logging.info('DB Timetable Plugin')
+
+def fetchTime(timecode):
     timecode = f"{timecode[6]+timecode[7]}:{timecode[8]+timecode[9]} Uhr"
     return timecode
 
-def path2Friendly(trainpath):
-    trainpath = trainpath.split("|")
-    return trainpath
+def fetchPath(train):
+    logging.info(train.tag)
+
+    #ich bin dumm, das geht net :()
+
+    return train
 
 def getTimetable(eva, date, time):
     infraGoUrl = f"https://apis.deutschebahn.com/db-api-marketplace/apis/timetables/v1/plan/{eva}/{date}/{time}"
@@ -20,29 +30,27 @@ def getTimetable(eva, date, time):
         }
     
     response = requests.get(infraGoUrl, headers=headers)
+    logging.info(f'HTTP Response from Timetables V1: {response}')
     data = ET.fromstring(response.text)
 
     timetable = []
 
     timetable.append(data.get("station"))
 
-    for s in data:
-        for t in s:
-            train = [
-                [
-                    [s[0].attrib.get("c"), s[1].attrib.get("l")],
-                    s[1].attrib.get("pp"),
-                    date2Friendly(s[1].attrib.get("pt"))
-                ],
-                path2Friendly(s[1].attrib.get("ppth"))
-            ]
-            timetable.append(train)
-
-    print(timetable)
+    for service in data:
+        train = [
+            [
+                [service[0].attrib.get("c"), service[1].attrib.get("l")],
+                service[1].attrib.get("pp"),
+                fetchTime(service[1].attrib.get("pt"))
+            ],
+            fetchPath(service)
+        ]
+        timetable.append(train)
     return timetable
 
 def createWidget():
-    timetable = getTimetable(8000157, 240201, 12)
+    timetable = getTimetable(8004094, 240205, 17)
     return render_template("timetable/index.html", stationName=timetable[0]), 200
 
 def createPlugin(plugin):

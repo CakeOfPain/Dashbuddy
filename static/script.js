bda38dedad384ef1e6c93554d316a803c5d2a5f8
riedl-sebastian <sebastian.riedl0405@gmail.com>
2024-02-09 12:08:44 +0100

    Open Edit via Library-Button

--- static/script.js
+++ static/script.js
@@ -30,7 +30,7 @@ slider.oninput = function () {
 function setAccentColor(color) {
     document.documentElement.style.setProperty("--accent-color", '#' + color);
 }
-
+// Settings menu
 var settingsMenu = document.getElementById("settingsMenu");
 function toggleSettingsMenu() {
     if (settingsMenu.style.display === "none") {
@@ -39,11 +39,14 @@ function toggleSettingsMenu() {
         settingsMenu.style.display = "none";
     }
 }
-
+// Problem: Settings und Library sind beide beim Start offen und werden übereinander ausgegeben: sehr unschön
+// Teilweise: Error 304, main.css Kommunikationsproblem mit Browser(Edge)
+// Änderung: Anstelle des Öffnens der Library, soll bei Click die URL zur ".../edit" geändert werden (Bearbeitungsmodus für die Widgets)
+// Code-Änderung: widgetLibrary.style.display="block" zu window.location.href="edit"
 var widgetLibrary = document.getElementById("widgetLibrary");
 function toggleWidgetMenu() {
     if (widgetLibrary.style.display === "none") {
-        widgetLibrary.style.display = "block";
+        window.location.href="edit";
     } else {
         widgetLibrary.style.display = "none";
     }

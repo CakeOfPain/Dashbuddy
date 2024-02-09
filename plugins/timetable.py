178bb7fb7fcf90498bd7af642bfdbb032b81da0a
Pascal Jedicke <pascal.jedicke@icloud.com>
2024-02-09 12:01:49 +0100

    Finished Backend for DB API Widget

--- plugins/timetable.py
+++ plugins/timetable.py
@@ -1,8 +1,10 @@
 from flask import request, render_template
+from markupsafe import Markup
+
+import datetime
 import logging
 import requests
 import xml.etree.ElementTree as ET
-
 
 logging.basicConfig(format='%(levelname)s - %(asctime)s - %(message)s', level=logging.INFO)
 logging.info('DB Timetable Plugin')
@@ -87,8 +89,31 @@ def getTimetable(eva, date, time):
     return timetable
 
 def createWidget():
-    timetable = getTimetable(8004094, 240208, 17)
-    return render_template("timetable/index.html", stationName=timetable[0]), 200
+    now = datetime.datetime.now()
+    # Hält die nächsten 8000 Jahre xD Bis dahin hat jemand anderes bestimmt ne bessere Lösung gefunden
+    dateNow = f'{str(now.year)[2]+str(now.year)[3]}{'{:02d}'.format(now.month)}{'{:02d}'.format(now.day)}'
+    timeNow = now.hour
+    timetable = getTimetable(8004094, dateNow, timeNow)
+    trains = ""
+
+    for i in range(len(timetable)):
+        if i == 0:
+            logging.info(f'Timetable Index 0 -> Station Name {timetable[0]}')
+        else:
+            trainType = timetable[i][1][0]
+            if trainType == "1":
+                logging.info(f'Timetable Index {i} -> Traintype {trainType}')
+                trains += f'<div class="header"><h4>{timetable[i][0][0][0]}{timetable[i][0][0][1]} {timetable[i][1][1]}</h4><p>Abfahrt {timetable[i][0][2]} Gleis {timetable[i][0][1]}</p></div>'
+            elif trainType == "2":
+                logging.info(f'Timetable Index {i} -> Traintype {trainType}')
+                #trains += f'<div class="header"><h4>{timetable[i][0][0][0]}{timetable[i][0][0][1]} {timetable[0]}</h4><p>Zug endet hier!</p></div>'
+            elif trainType == "3":
+                logging.info(f'Timetable Index {i} -> Traintype {trainType}')
+                trains += f'<div class="header"><h4>{timetable[i][0][0][0]}{timetable[i][0][0][1]} {timetable[i][1][2]}</h4><p>Abfahrt {timetable[i][0][2]} Gleis {timetable[i][0][1]}</p></div>'
+    
+    data = Markup(f'<div class="train">{trains}</div>')
+
+    return render_template("timetable/index.html", stationName=timetable[0], timetableData = data), 200
 
 def createPlugin(plugin):
     plugin.name("timetable")

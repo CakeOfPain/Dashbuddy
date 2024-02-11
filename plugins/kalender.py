04560020682d72a7a6b7ca612d7ed2de7506f2c2
riedl-sebastian <sebastian.riedl0405@gmail.com>
2024-02-11 11:57:53 +0100

    Calendar Layout reworked (not done with presentation at all)

--- plugins/kalender.py
+++ plugins/kalender.py
@@ -2,29 +2,35 @@ from datetime import date
 from datetime import datetime
 import calendar
 from flask import render_template
-
-def getDate():
-    aktuellesD=date.today()
-    furweekday=str(aktuellesD)
-    return furweekday, 200
     
-def getWeekDay():
-    wochentage=["Montag","Dienstag","Mittwoch","Donnerstag","Freitag","Samstag","Sonntag"]
-    tag=date.weekday()
-    return wochentage[tag], 200
-
 def getCalender():
-    kalenderseite=calendar.HTMLCalendar(calendar.MONDAY)
-    aus=kalenderseite.formatmonth(datetime.now().date().year,datetime.now().date().month)
-    return(aus), 200
+    aktuellesD=datetime.now()
+    wochentag=aktuellesD.strftime("%A")
+    datum=aktuellesD.strftime("%d.%m.%Y")
+    tagdatum=f"{wochentag}, der {datum}"
 
-def getContent():
-    return render_template("calendar/index.html"), 200
+    heute=datetime.now()
+    jahr=heute.year
+    monat=heute.month
+    tag=heute.day
+
+    kal_monat=calendar.monthcalendar(jahr,monat)
+    for woche in kal_monat:
+        for i, t in enumerate(woche):
+            if t== tag:
+                woche[i]=str(t).lstrip("0")
+    wochentage=["MO","DI","MI","DO","FR","SA","SO"]
+    wochentage_string=" ".join(wochentage)
+
+    aktueller_wochentag=calendar.weekday(jahr,monat,tag)
+    markierter_tag=wochentage[aktueller_wochentag]+"*"
+
+    kal_monat_string="\n".join(" ".join(markierter_tag if t==tag else str(t) for t in woche) for woche in kal_monat)
+
+    return render_template("calendar/index.html", weekDays=wochentage_string, weeks=kal_monat_string, dateDay=tagdatum),200
 
 def createPlugin(plugin):
     plugin.name('calendar')
     plugin.describe('Zeigt den Kalender')
-    plugin.api('get-Date',getDate)
-    plugin.api('get-Weekday',getWeekDay)
     plugin.api('get-Calender',getCalender)
-    plugin.content(getContent)
+    plugin.content(getCalender)
